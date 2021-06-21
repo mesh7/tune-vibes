5a59e13b29ab58a3293997e82518cf71a9f81899
mesh7 <meshram.shubham27@gmail.com>
2021-06-21 18:40:20 +0530

    initial commit for application

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TuneVibes msg="OCEAN" />
+  </div>
+</template>
+
+<script>
+import TuneVibes from "./components/TuneVibes.vue";
+
+export default {
+  name: "App",
+  components: {
+    TuneVibes,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
